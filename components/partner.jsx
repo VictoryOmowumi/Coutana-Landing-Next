e87a5df8003fbcdef22df381d50f44b945cd9ca8
victory balogun <victorybalogun@gmail.com>
2022-11-18 13:00:49 +0100

    update

--- components/partner.jsx
+++ components/partner.jsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 export const Partner = () => {
   return (
-    <div className="h-[60vh] w-full flex justify-center items-center bg-[url('../assets/banner-2.jpg')] bg-blend-overlay bg-cover bg-no-repeat bg-fixed bg-center  " >
+    <div className="h-[60vh] w-full flex justify-center items-center bg-[url('../assets/banner-2.jpg')]  bg-cover bg-no-repeat bg-fixed bg-left-bottom sm:bg-contain  " >
       <div className=" mx-auto p-4" id="partner" >
         <div className=" col-span-1 text-center " id="partner-header">
           <p className=" text-6xl text-coutana-gray-200 ">
             Grow your brand today with{" "}
             <span className="text-[#fff]">Coutana</span>{" "}
           </p>
-          <p className="text-coutana-gray-200 text-base  ">
+          <p className="text-coutana-gray-200 text-2xl ">
             With our platform, you can reach millions of customers and grow your
             business. <br /> <span className="text-[#ffffff]">Partner with us.</span>
             
